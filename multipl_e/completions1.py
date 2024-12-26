207ea1a20f84d9f3c221a7126eed318863e8fc43
Towhidul Tonmoy <55044336+towhidultonmoy@users.noreply.github.com>
2024-12-26 23:40:43 +0600

    Update completions1.py

--- multipl_e/completions1.py
+++ multipl_e/completions1.py
@@ -115,7 +115,7 @@ def make_main(args, model_name, gen_completions):
         problems = datasets.load_dataset(
             "json", data_files=args.dataset, split="train")
     else:
-        problems = datasets.load_dataset(f"Dataset678/{args.root_dataset}-{args.lang}" split="test")
+        problems = datasets.load_dataset(f"Dataset678/{args.root_dataset}-{args.lang}" ,split="test")
         # problems = datasets.load_dataset(
         #     "Dataset678/humaneval-java" ,split="test"
         # )
